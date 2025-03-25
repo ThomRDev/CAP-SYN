39a851db4e7f16ebc7bf25fa0c7462743d30584f
ThomRdev <thom.maurick.r@gmail.com>
2025-03-25 13:10:04 -0500

    cambios basename

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById("root")!).render(
   <>
     <Provider store={store}>
       <UIProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/CAP-SYN">
           <AppRouter />
         </BrowserRouter>
       </UIProvider>
